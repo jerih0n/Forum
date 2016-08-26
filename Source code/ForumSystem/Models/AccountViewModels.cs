557c88417679275a9f6af8b3ce793434e45b2ab9
Dimitur Botev <botev_dimitur@abv.bg>
2016-08-26 12:56:33 +0300

    New commit
    Changing the registration form,home and shared views

--- Source code/ForumSystem/Models/AccountViewModels.cs
+++ Source code/ForumSystem/Models/AccountViewModels.cs
@@ -65,12 +65,21 @@ namespace ForumSystem.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(50)]
+        [Display(Name ="Nickname")]
+        public string Nickname { get; set; }
+
+        [StringLength(150)]
+        [Display(Name ="Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -89,7 +98,7 @@ namespace ForumSystem.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
