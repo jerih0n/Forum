3b7e7c1e2dc9000406de80dbae3b848b2286cb15
Dimitur Botev <botev_dimitur@abv.bg>
2016-08-26 13:16:53 +0300

    Adding Question Model

---
+++ Source code/ForumSystem/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ForumSystem.Models
+{
+    public class Question
+    {
+        [Key]
+        public int QuestionId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string QuestionTitle { get; set;}
+        [Required]
+        [StringLength(1000)]
+        public string QuestionBody { get; set; }
+
+        public DateTime Date { get { return DateTime.Now.Date; } }
+        
+        public ApplicationUser Author { get; set;}
+
+    }
+}
